4f6fddeaf8abd4d59a8a77b6884990c4567cbebb
JAMESMUTISYA1 <ravetmu@gmail.com>
2025-03-29 12:42:01 +0300

    links

--- app/(admin-group)/admin/vehicles/page.jsx
+++ app/(admin-group)/admin/vehicles/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
-import { db } from '../../../Shared/Firebaseconfig';
+import { db } from '../../../../Shared/Firebaseconfig';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
--- app/(admin-group)/layout.jsx
+++ app/(admin-group)/layout.jsx
@@ -1,4 +1,4 @@
-//import Sidebar from './login/Sidebar';
+import Sidebar from './admin/Sidebar';
 import Header from '@/components/Headeradmin';
 import ProtectedRoute from '@/components/ProtectedRoute';
 
@@ -6,8 +6,8 @@ export default function AdminLayout({ children }) {
   return (
     <ProtectedRoute>
       <div className="flex h-screen bg-gray-100">
-        {/* Sidebar <Sidebar />- Always visible on desktop */}
-        
+        {/* Sidebar - Always visible on desktop */}
+        <Sidebar />
 
         {/* Main content */}
         <div className="flex-1 flex flex-col overflow-hidden">
