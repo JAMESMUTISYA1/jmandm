449751d7b80d76262695ec3ceb09b934794ad5e1
JAMESMUTISYA1 <ravetmu@gmail.com>
2025-03-29 14:38:56 +0300

    hh

--- app/(admin-group)/layout.jsx
+++ app/(admin-group)/layout.jsx
@@ -26,25 +26,26 @@ export default function AdminLayout({ children }) {
     <ProtectedRoute>
       <html lang="en" className="h-full">
         <body className="h-full">
+        <Header showMenu={!isAdminDashboard} showLogout={isLoggedIn} />
           <div className="flex h-screen bg-gray-100">
-            {/* Sidebar - Hidden on /admin route */}
-            {!isAdminDashboard && <Sidebar isLoggedIn={isLoggedIn} />}
-
+            <div> {!isAdminDashboard && <Sidebar isLoggedIn={isLoggedIn} />}</div>
+           
             {/* Main content area */}
             <div className={`flex-1 flex flex-col overflow-hidden ${!isAdminDashboard ? '' : 'pl-0'}`}>
-              <Header showMenu={!isAdminDashboard} showLogout={isLoggedIn} />
+             
               
               <main className="flex-1 overflow-y-auto p-4 md:p-6">
                 {children}
               </main>
               
-              <footer className="bg-gray-800 py-4 px-6">
+             
+            </div>
+          </div>
+          <footer className="bg-gray-800 py-4 px-6">
                 <p className="text-sm text-white text-center">
                   © {new Date().getFullYear()} Developed by JMandM tech James Mutisya
                 </p>
               </footer>
-            </div>
-          </div>
         </body>
       </html>
     </ProtectedRoute>
