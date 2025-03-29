f4520cadadf3bed47f0e32661910a3d09f81d512
JAMESMUTISYA1 <ravetmu@gmail.com>
2025-03-29 14:29:33 +0300

    header

--- components/Headeradmin.jsx
+++ components/Headeradmin.jsx
@@ -1,31 +1,19 @@
 // components/Headeradmin.jsx
 'use client';
 
-import { getAuth, signOut } from 'firebase/auth';
-import { app } from '../Shared/Firebaseconfig';
-import { useRouter } from 'next/navigation';
-
 export default function Header({ showMenu = true, showLogout = false }) {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      router.push('/login');
-    } catch (error) {
-      console.error('Logout error:', error);
-    }
-  };
-
+ 
   return (
-    <header className="bg-indigo-600 shadow-sm">
+    <header className="bg-indigo-600  h-40 shadow-sm">
       <div className="flex items-center justify-between p-4">
-        {showMenu && (
+
+      <div className="g-6" > {showMenu && (
           <button className="lg:hidden">
             {/* Your menu icon */}
           </button>
         )}
+        <p>ADMIN</p></div>
+       
          <div className="flex items-center">
            
               <img
@@ -36,15 +24,7 @@ export default function Header({ showMenu = true, showLogout = false }) {
               <span className="ml-2 text-xl text-black font-semibold">JM and M</span>
         
           </div>
-        <div className="flex-1"></div>
-        {showLogout && (
-          <button 
-            onClick={handleLogout}
-            className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
-          >
-            Logout
-          </button>
-        )}
+
       </div>
     </header>
   );
